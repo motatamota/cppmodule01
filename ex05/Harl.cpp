6fe84a07c8599c434e40a192e9b268d1601c14cc
motatamota <tashiget@student.42tokyo.jp>
2024-10-18 00:46:28 +0900

    tset

---
+++ ex05/Harl.cpp
@@ -0,0 +1,49 @@
+#include "Harl.h"
+
+Harl::Harl()
+{
+
+}
+
+Harl::~Harl()
+{
+
+}
+
+void	Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info()
+{
+	std::cout <<  "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	int	n;
+	void	(Harl::*ptr_complain[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	n = 0;
+	while (n < 4)
+	{
+		if (levels[n] == level)
+		{
+			(this->*ptr_complain[n])();
+			break;
+		}
+		n++;
+	}
+}
