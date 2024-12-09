506368d3a97e53c0208de50e1c08c6c089313dea
tashiget <tashiget@student.42tokyo.jp>
2024-12-09 21:48:47 +0900

    test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,14 +7,15 @@ int	main()
 	std::string	*stringPTR;
 	std::string	&stringREF = str;
 
-	str = "HI THIS IS BRAIN";
 	stringPTR = &str;
+	str = "HI THIS IS BRAIN";
 	std::cout << "Memorry" << std::endl;
 	std::cout << "variable: " << &str << std::endl;
 	std::cout << "pointer: " << &stringPTR << std::endl;
 	std::cout << "reference: " << &stringREF << std::endl << std::endl;
 	std::cout << "value" << std::endl;
 	std::cout << "variable: " << str << std::endl;
-	std::cout << "pointer: " << *stringPTR << std::endl;
+	std::cout << "pointer: " << stringPTR << std::endl;
+	std::cout << "*pointer: " << *stringPTR << std::endl;
 	std::cout << "reference: " << stringREF << std::endl;
 }
