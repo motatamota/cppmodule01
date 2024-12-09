e66da533a5ff5f6901ddd82e7f03a5bc27994c7c
tashiget <tashiget@student.42tokyo.jp>
2024-12-09 21:44:03 +0900

    test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,10 +5,17 @@ int	main()
 	Zombie	zombie;
 	Zombie	*zombies;
 	int		n;
+	int		l;
 
 	zombie.setName("father");
 	std::cout << "How many zombies?: ";
 	std::cin >> n;
 	zombies = zombie.zombieHorde(n, "child");
+	l = 0;
+	while (l < n)
+	{
+		zombies[l].announce();
+		l++;
+	}
 	delete [] zombies;
 }
